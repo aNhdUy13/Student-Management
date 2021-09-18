89bfb9192edb9717c762e30a2a817096502f6072
aNhdUy13 <73863929+aNhdUy13@users.noreply.github.com>
2021-09-18 08:27:02 +0700

    update Student file

---
+++ view/main.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main Page</title>
+    <link rel="stylesheet" href="css/normal_layout.css" type="text/css">
+
+</head>
+
+<body>
+</body>
+<div class="container ">
+    <!-- Header -->
+    <div class="header">
+        <div class="greeting ">
+            <h1> ATN Shop </h1>
+        </div>
+    </div>
+    
+      <div class="navigation_bar ">    
+                <nav>
+                    <form method="GET" action=""></form>
+                        <ul>
+                            <li>
+                                <a href="/home">Product</a>
+                            </li>
+                            <li>
+                                <a href="/manage">Product Management</a>
+                            </li>
+                            <!-- <input class="get_text_search" type="text" name="search" placeholder="Input Product's Name ...">
+                            <input placeholder="Search" type="submit" name="submit"> -->
+                        </ul>
+                    </form>
+                </nav>
+        </div>
+
+    <!-- (End) Header -->
+
+
+    <!-- Content -->
+    <div class="main_Content">
+
+        <div class="intro_image_content">
+            <img>
+        </div>
+
+        <div class="product_content">
+                <div class="product_content_detail">
+                    <img src="https://media.istockphoto.com/photos/brown-teddy-bear-isolated-in-front-of-a-white-background-picture-id909772478?k=6&m=909772478&s=612x612&w=0&h=X55jzpsKboa_jUjbEN8eqAn0gjt696ldbeJMEqmNrcU=">
+
+
+                    <button class="product_price"></button>
+                </div>
+
+                <div class="product_content_detail">
+                    <img src="https://i.pinimg.com/originals/a2/24/86/a22486e0ad61ddfc35d3eeb18de39da2.jpg">
+                    <button class="product_price"></button>
+                </div>
+
+                <div class="product_content_detail">
+                    <img src="https://images-na.ssl-images-amazon.com/images/I/71mtU3dwbSL._AC_SX425_.jpg">
+                    <button class="product_price"></button>
+                </div>
+
+                <div class="product_content_detail">
+                    <img src="https://mma.prnewswire.com/media/1485871/Moose_Toys_Fall_Guys.jpg?p=publish&w=950">
+
+                    <button class="product_price"></button>
+                </div>
+
+                <div class="product_content_detail">
+                    <img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F38%2F2019%2F06%2F12233920%2F550_102389656.jpg">
+
+                    <button class="product_price"></button>
+                </div>
+
+                <div class="product_content_detail">
+                    <img src="https://images.unsplash.com/photo-1603356033288-acfcb54801e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTZ8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80">
+
+                    <button class="product_price"></button>
+                </div>
+        </div>
+       
+
+
+    </div>
+
+    <!-- (End) Content -->
+
+    <!-- Footer -->
+    <div class="footer">
+        <footer>Copyright <a>&copy</a> 2020 </footer>
+    </div>
+
+    <!-- (End) Footer -->
+
+</div>
+
+
+</html>
