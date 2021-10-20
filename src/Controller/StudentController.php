66d95dd572d17fb24533fd5f2dd3f956a5457be8
Duy Anh Nguyen <73863929+aNhdUy13@users.noreply.github.com>
2021-10-20 12:48:07 +0700

    update appearacne

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Student;
+use App\Form\StudentType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -55,7 +56,27 @@ class StudentController extends AbstractController
 
     #[Route('/student/add', name: 'student_add')]
     public function addStudentAction(Request $request){
-        
+        $student = new Student();
+        $form = $this->createForm(StudentType::class, $student);
+        $form -> handleRequest($request);
+
+        if($form -> isSubmitted() && $form -> isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+
+            $manager->persist($student);
+
+            $manager->flush();
+
+            $this -> addFlash('Success', "Student Added Success!");
+
+            return $this -> redirectToRoute('student_index');
+        }
+
+        return $this -> render('student/add.html.twig',
+        [
+            'form' => $form->createView(),
+        ]);
+
     }
 
     #[Route('student/update/{id}', name: 'student_update')]
--- src/Form/StudentType.php
+++ src/Form/StudentType.php
@@ -2,16 +2,79 @@
 
 namespace App\Form;
 
+use App\Entity\Course;
+use App\Entity\Sclass;
+use App\Form\SclassType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class StudentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+            ->add('name', TextType::class,
+            [
+                'label' => "Name",
+                'required' => true
+            ])
+
+            ->add('dob',DateType::class,
+            [
+                'label' => "Date of Birth",
+                'widget' => 'single_text'
+            ])
+
+            ->add('gender', ChoiceType::class,
+            [
+                'choices' =>
+                [
+                    "Male" => "Male",
+                    "Female" => "Female",
+                    "Other" => "Other"
+                ],
+                'required' => true
+            ])
+
+            ->add('phoneNumber', TextType::class,
+            [
+                'label' => "Phone Number"
+            ])
+
+            ->add('image', FileType::class,
+            [
+                'label' => "Image",
+                'data_class' => null,
+                'required' => is_null($builder->getData() -> getImage())
+
+            ])
+
+            ->add('class', EntityType::class,
+            [
+                'label' => "Class",
+                'class' => Sclass::class,
+                'choice_label' => "name",
+
+                'expanded' => false,        // true: checkbox, false: dropdown list
+                'multiple' => false,        // true: Có thể chọn nhiều, false: Chỉ chọn 1 
+
+            ])
+
+            -> add('course', EntityType::class,
+            [
+                'label' => "Course",
+                'class' => Course::class,
+                'choice_label' => "name",
+
+                'multiple' => true,   // true: Có thể chọn nhiều, false: Chỉ chọn 1 
+                'expanded' => false,  // true: checkbox, false: dropdown list
+            ])
         ;
     }
 
