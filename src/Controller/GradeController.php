983f6a1f528c2cf24a4e6384c915fff4de2d295f
hoang4326 <84314103+hoang4326@users.noreply.github.com>
2021-10-21 15:32:27 +0700

    update view function in Grade

--- src/Controller/GradeController.php
+++ src/Controller/GradeController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Grade;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class GradeController extends AbstractController
 {
-    #[Route('/grade', name: 'grade')]
-    public function index(): Response
-    {
-        return $this->render('grade/index.html.twig', [
-            'controller_name' => 'GradeController',
+    #[Route('/grade', name: 'grade_view')]
+    public function viewGrade(){
+        $grade = $this->getDoctrine()->getRespority(Grade::class);
+
+        return $this->render('grade/view.html.twig',[
+            'grade' => $grade,
         ]);
     }
+
+    #[Route('/grade/detail/{id}', name: 'grade_detail')]
+    public function gradeDetail($id){
+        $grade = $this->getDoctrine()->getRespority(Grade::class)-> findAll($id);
+
+        return $this->render('grade/detail.html.twig',[
+            'grade' => $grade,
+        ]);
+    }
+
 }
