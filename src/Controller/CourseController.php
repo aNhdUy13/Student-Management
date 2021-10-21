6a86914287a5f9e5cee9c0d0d964627629015678
tan <dinovu285@gmail.com>
2021-10-22 06:26:56 +0700

    done layout course page

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,17 +2,60 @@
 
 namespace App\Controller;
 
+use App\Entity\;
+use App\Form\CourseType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
+
 
 class CourseController extends AbstractController
 {
-    #[Route('/course', name: 'course')]
-    public function index(): Response
-    {
-        return $this->render('course/index.html.twig', [
-            'controller_name' => 'CourseController',
-        ]);
+    #[Route('/course', name: 'course_index')]
+    public function courseIndex(){
+        $courses = $this->getDoctrine()->getRepository(Course::class)->findAll();
+        return $this->render(
+            'course/index.html.twig',
+            [
+                'courses' => $courses
+            ]
+        );
     }
+
+    #[Route('/course/detail/{id}',name : 'course_detail')]
+    public function courseDetail($id){
+        $course = $this->getDoctrine()->getRepository(Course::class)->find($id);
+        if ($course == null){
+            $this->addFlash('Error','Course not exist !');  
+            return $this->redirectToRoute('course_index');
+        }
+        else{
+            return $this->render(
+                'course/detail.html.twig',
+                [
+                    'course' => $course
+                ]
+            );
+        }
+    }
+
+    #[Route('/course/delete/{id}', name : 'course_delete')]
+    public function courseDelete($id){
+
+    } 
+
+    #[Route('/course/add', name : 'course_add')]
+    public function courseAdd(Request $request){
+
+    }
+
+    #[Route('course/update/{id}', name : 'course_update')]
+    public function courseUpdate(Request $request, $id){
+        
+    }
+
+
 }
