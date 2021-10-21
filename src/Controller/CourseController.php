03ccfa500f66a5535e9b61019a447b402af89814
tan <dinovu285@gmail.com>
2021-10-22 06:27:30 +0700

    fix use

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\;
+use App\Entity\Course;
 use App\Form\CourseType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
