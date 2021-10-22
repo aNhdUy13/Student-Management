ed5c2b3c80e6c59e931e50d5fa0acb098cb72628
hoang4326 <84314103+hoang4326@users.noreply.github.com>
2021-10-22 12:58:13 +0700

    Update GradeType

--- src/Form/GradeType.php
+++ src/Form/GradeType.php
@@ -3,18 +3,39 @@
 namespace App\Form;
 
 use App\Entity\Grade;
+use App\Entity\Student;
+use App\Entity\Subject;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class GradeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('grade')
-            ->add('subject')
-            ->add('student')
+            ->add('grade',IntegerType::class,[
+                'label' => 'Grade',
+                'required' => true
+            ])
+            ->add('subject',EntityType::class,[
+                'label' => 'Subject',
+                'class' => Subject::class,
+                'choice_label' => "name",
+
+                'multiple' => false,
+                'expanded' => false
+            ])
+            ->add('student',EntityType::class,[
+                'label' => 'Student',
+                'class' => Student::class,
+                'choice_label' => "name",
+
+                'multiple' => false,
+                'expanded' => false
+            ])
         ;
     }
 
